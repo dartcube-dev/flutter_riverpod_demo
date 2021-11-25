16d03a62d8ddd1372ec2617e82c949d7cd9aa5ce
samrat19 <babinsamrat123@gmail.com>
2021-11-25 19:32:09 +0530

    adds APIKey

--- lib/logic/news_manager.dart
+++ lib/logic/news_manager.dart
@@ -1,10 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod_demo/api_key.dart';
 import 'news_resposne_model.dart';
 
 class NewsManager{
   Future<NewsResponseModel> getMovies({String? newsCategory})async{
     Dio _dio = Dio();
-    var response = await _dio.get('https://newsapi.org/v2/top-headlines?country=in&category=$newsCategory'+'&apiKey=');
+    var response = await _dio.get('https://newsapi.org/v2/top-headlines?country=in&category=$newsCategory'+'&apiKey=$apiKey');
     return NewsResponseModel.fromJson(response.data);
-  } 
+  }
 }
