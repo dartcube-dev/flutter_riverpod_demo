6bce3d51999b60a07b383f20a55a078f0db6b422
samrat19 <babinsamrat123@gmail.com>
2021-11-26 00:18:35 +0530

    makes some changes

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -15,7 +15,7 @@ class NewsScreen extends ConsumerWidget {
         widgetRef.watch(newsResponseProvider(refreshedValue.url));
 
     return Scaffold(
-      appBar: AppBar(title: Text(refreshedValue.url),),
+      appBar: AppBar(title: Text(refreshedValue.url.toUpperCase()),),
       body: newsResponseAsyncValue.map(data: (_) {
         return RefreshIndicator(
           onRefresh: () {
