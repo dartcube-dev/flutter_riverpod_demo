acf143ee74040eae9800de8a5ba075fb9acdfcd0
samrat19 <babinsamrat123@gmail.com>
2021-11-26 00:31:28 +0530

    handles some null errors

--- lib/screens/widget/news_widget.dart
+++ lib/screens/widget/news_widget.dart
@@ -10,9 +10,21 @@ class NewsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      shrinkWrap: true,
       itemBuilder: (_,index){
-        return ListTile(
-          title: Text(newsResponse.articles![index].title!),
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: newsResponse.articles![index].urlToImage == null?const SizedBox(
+              height: 80,
+              width: 100,
+            ):Image.network(newsResponse.articles![index].urlToImage!,height: 80,width: 100,),
+            title: newsResponse.articles![index].title == null?const Text(''):Text(newsResponse.articles![index].title!,style: Theme.of(context).textTheme.subtitle1!.copyWith(
+              fontWeight: FontWeight.bold,
+            ),),
+            subtitle: newsResponse.articles![index].description == null?const Text(''):Text(newsResponse.articles![index].description!,style: Theme.of(context).textTheme.bodyText1),
+            isThreeLine: true,
+          ),
         );
       },
       itemCount: newsResponse.articles!.length,
